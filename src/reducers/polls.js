32468d5d4be0170d45bb1532f9a7e3bb616f2d26
opauloh <paulo.scape@gmail.com>
2020-07-17 09:04:09 -0300

    reducers

---
+++ src/reducers/polls.js
@@ -0,0 +1,32 @@
+import { ADD_POLL, RECEIVE_POLLS } from '../actions/polls';
+import { ADD_ANSWER } from '../actions/answers';
+
+
+export default function polls(state = {}, action) {
+  switch (action.type) {
+    case RECEIVE_POLLS:
+      return {
+        ...state,
+        ...action.polls
+      }
+    case ADD_POLL:
+      return {
+        ...state,
+        [action.poll.id]: action.poll
+      }
+    case ADD_ANSWER:
+      const { answer, id, authedUser } = action;
+      const poll = state[id]
+      const votesKey = answer + 'Votes';
+
+      return {
+        ...state,
+        [action.id]: {
+          ...poll,
+          [votesKey]: poll[votesKey].concat([authedUser])
+        }
+      }
+    default:
+      return state
+  }
+}
